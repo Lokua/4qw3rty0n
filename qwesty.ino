bc986fc9427afe4fbd767a3052b35683f5d44cff
Joshua Kleckner <lokua@lokua.net>
2020-04-30 00:03:05 -0500

    feat: ps2 input working

--- qwesty.ino
+++ qwesty.ino
@@ -1,31 +1,51 @@
+#include <PS2Keyboard.h>
+
 const uint8_t CHANNEL = 0;
 const uint8_t NOTE_OFF = 128;
 const uint8_t NOTE_ON = 144;
 const uint8_t DEFAULT_VELOCITY = 127;
+const uint8_t KEYS[] = {65, 71, 53, 66, 68, 73, 69, 57, 63, 64, 49, 67, 52, 54, 55, 56, 58, 59, 60, 74, 72, 51, 70, 50, 62, 61};
 
-uint8_t demoNotes[] = {63, 67, 69, 70, 74, 75};
-uint16_t demoDuration = 118;
-uint8_t demoClock = 0;
-uint8_t note = demoNotes[0];
+// Arduino cannot print to the Serial Monitor when using MIDI Baud (bits per second)?
+const bool DEBUG_PS2 = true;
+const uint16_t BAUD = DEBUG_PS2 ? 9600 : 31250;
+
+const uint8_t PS2_CLOCK_PIN = 3;
+const uint8_t PS2_DATA_PIN = 4;
+
+PS2Keyboard keyboard;
+bool noteOns[128] = {}; 
 
 void setup() {
-  Serial.begin(31250);
-  sendAllNotesOff(CHANNEL);
+  Serial.begin(BAUD);
+  keyboard.begin(PS2_DATA_PIN, PS2_CLOCK_PIN);
+  
+  if (!DEBUG_PS2) {
+    sendAllNotesOff(CHANNEL);
+  }
 }
 
 void loop() {
-  sendNoteOn(CHANNEL, note, DEFAULT_VELOCITY);
-  delay(demoDuration);
+  if (keyboard.available()) {
+    byte value = keyboard.read() - '0';
 
-  sendNoteOff(CHANNEL, note);
-  delay(demoDuration);
-
-  demoClock = (demoClock + 1) % sizeof(demoNotes);
-  note = demoNotes[demoClock];
+    if (!DEBUG_PS2 && isAlphaKey(value)) {
+      sendNoteOn(CHANNEL, value);
+      // we will obviously not do this delay in real implementation 
+      delay(118);
+      sendNoteOff(CHANNEL, value);
+    } else if (DEBUG_PS2) {
+      Serial.println(value);
+    }
+  }
 }
 
 void sendNoteOn(uint8_t channel, uint8_t note, uint8_t velocity) {
   sendMIDI(NOTE_ON + channel, note, velocity);
+}
+
+void sendNoteOn(uint8_t channel, uint8_t note) {
+  sendMIDI(NOTE_ON + channel, note, DEFAULT_VELOCITY);
 }
 
 void sendNoteOff(uint8_t channel, uint8_t note) {
@@ -52,4 +72,8 @@ void debugMIDI(uint8_t status, uint8_t data1, uint8_t data2) {
   Serial.print(", ");
   Serial.print(data2);
   Serial.print("\n");
+}
+
+bool isAlphaKey(byte key) {
+  return key > 48 && key < 75;
 }
