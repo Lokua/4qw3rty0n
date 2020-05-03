cb5838aac6e5fa0af2b59e1525fbadec2a8ac930
Joshua Kleckner <lokua@lokua.net>
2020-05-02 20:08:18 -0500

    feat: improve key detection

--- qwesty.ino
+++ qwesty.ino
@@ -1,51 +1,168 @@
-#include <PS2Keyboard.h>
+#include <PS2KeyAdvanced.h>
+#include "constants.h"
 
-const uint8_t CHANNEL = 0;
-const uint8_t NOTE_OFF = 128;
-const uint8_t NOTE_ON = 144;
-const uint8_t DEFAULT_VELOCITY = 127;
-const uint8_t KEYS[] = {65, 71, 53, 66, 68, 73, 69, 57, 63, 64, 49, 67, 52, 54, 55, 56, 58, 59, 60, 74, 72, 51, 70, 50, 62, 61};
+// when on, will print MIDI statements to the Serial Monitor instead of
+// actually sending MIDI since Arduino Uno only has a single hardware Serial and cannot
+// println under the MIDI BUAD rate
+#define DEBUG_PS2 1
 
-// Arduino cannot print to the Serial Monitor when using MIDI Baud (bits per second)?
-const bool DEBUG_PS2 = true;
+#define NOTE_OFF 128
+#define NOTE_ON 144
+#define DEFAULT_VELOCITY 127
+#define DATA_PIN 4
+#define IRQ_PIN  3
+
+PS2KeyAdvanced keyboard;
+
 const uint16_t BAUD = DEBUG_PS2 ? 9600 : 31250;
 
-const uint8_t PS2_CLOCK_PIN = 3;
-const uint8_t PS2_DATA_PIN = 4;
-
-PS2Keyboard keyboard;
-bool noteOns[128] = {}; 
+struct programState
+{
+  uint8_t channel = 0;
+  bool heldKeys[255] = {};
+  bool hold = false;
+  uint8_t scaleIndex = 0;
+};
+typedef struct programState ProgramState;
+ProgramState state;
 
 void setup() {
   Serial.begin(BAUD);
-  keyboard.begin(PS2_DATA_PIN, PS2_CLOCK_PIN);
-  
+  keyboard.begin(DATA_PIN, IRQ_PIN);
+  keyboard.setNoRepeat(1);
+
   if (!DEBUG_PS2) {
-    sendAllNotesOff(CHANNEL);
+    sendAllNotesOff(state.channel);
   }
+
+  /* see `util.js` for why this commented out code is here */
+  //  for (uint8_t i = 0; i < sizeof(KEYS); i++) {
+  //    Serial.println(KEYS[i]);
+  //  }
 }
 
 void loop() {
-  if (keyboard.available()) {
-    byte value = keyboard.read() - '0';
-
-    if (!DEBUG_PS2 && isAlphaKey(value)) {
-      sendNoteOn(CHANNEL, value);
-      // we will obviously not do this delay in real implementation 
-      delay(118);
-      sendNoteOff(CHANNEL, value);
-    } else if (DEBUG_PS2) {
-      Serial.println(value);
-    }
+  if (!keyboard.available()) {
+    return;
   }
+
+  uint16_t key = keyboard.read();
+  uint8_t keyCode = key & 0xFF;
+
+  Serial.print("keyCode");
+  Serial.print(isKeyUp(key) ? "(keyup): " : ": ");
+  Serial.println(keyCode);
+
+  if (isKeyUp(key)) {
+    if (isNote(keyCode) && !state.hold) {
+      sendNoteOff(keyCode);
+      state.heldKeys[keyCode] = false;
+    }
+  } else if (isNote(keyCode) && !state.heldKeys[keyCode]) {
+    Serial.print("index: ");
+    Serial.println(KEY_CODE_TO_INDEX[keyCode]);
+    sendNoteOn(keyCode);
+    state.heldKeys[keyCode] = true;
+  } else if (isScale(keyCode)) {
+    Serial.println("scale");
+  } else if (isHold(keyCode)) {
+    Serial.println("hold");
+  } else if (isOctave(keyCode)) {
+    Serial.println("octave");
+  }
+}
+
+bool isKeyUp(uint16_t key) {
+  //  Serial.print("keyup debug: ");
+  //  Serial.print(key >> 8, HEX);
+  //  Serial.print(" ");
+  uint16_t status_ = key >> 8;
+  return status_ == 0x80 || status_ == 0x81;
+}
+
+// see createKeyToIndex.js for generating these conditions
+bool isNote(uint8_t keyCode) {
+  return keyCode == 28 ||
+         keyCode == 30 ||
+         keyCode == 48 ||
+         keyCode == 49 ||
+         keyCode == 50 ||
+         keyCode == 51 ||
+         keyCode == 52 ||
+         keyCode == 53 ||
+         keyCode == 54 ||
+         keyCode == 55 ||
+         keyCode == 56 ||
+         keyCode == 57 ||
+         keyCode == 59 ||
+         keyCode == 60 ||
+         keyCode == 61 ||
+         keyCode == 62 ||
+         keyCode == 64 ||
+         keyCode == 65 ||
+         keyCode == 66 ||
+         keyCode == 67 ||
+         keyCode == 68 ||
+         keyCode == 69 ||
+         keyCode == 7 ||
+         keyCode == 70 ||
+         keyCode == 71 ||
+         keyCode == 72 ||
+         keyCode == 73 ||
+         keyCode == 74 ||
+         keyCode == 75 ||
+         keyCode == 76 ||
+         keyCode == 77 ||
+         keyCode == 78 ||
+         keyCode == 79 ||
+         keyCode == 80 ||
+         keyCode == 81 ||
+         keyCode == 82 ||
+         keyCode == 83 ||
+         keyCode == 84 ||
+         keyCode == 85 ||
+         keyCode == 86 ||
+         keyCode == 87 ||
+         keyCode == 88 ||
+         keyCode == 89 ||
+         keyCode == 90 ||
+         keyCode == 91 ||
+         keyCode == 92 ||
+         keyCode == 93 ||
+         keyCode == 94 ||
+         keyCode == 95;
+}
+
+bool isAlphaKey(uint8_t keyCode) {
+  return keyCode >= PS2_KEY_A && keyCode <= PS2_KEY_Z;
+}
+
+bool isNumericKey(uint8_t keyCode) {
+  return keyCode >= PS2_KEY_0 && keyCode <= PS2_KEY_9;
+}
+
+bool isScale(uint8_t keyCode) {
+  return keyCode >= PS2_KEY_F1 && keyCode <= PS2_KEY_F12;
+}
+
+bool isHold(uint8_t keyCode) {
+  return keyCode == PS2_KEY_CAPS;
+}
+
+bool isOctave(uint8_t keyCode) {
+  return keyCode == PS2_KEY_UP_ARROW || keyCode == PS2_KEY_DN_ARROW;
+}
+
+void sendNoteOn(uint8_t note) {
+  sendNoteOn(state.channel, note, DEFAULT_VELOCITY);
 }
 
 void sendNoteOn(uint8_t channel, uint8_t note, uint8_t velocity) {
   sendMIDI(NOTE_ON + channel, note, velocity);
 }
 
-void sendNoteOn(uint8_t channel, uint8_t note) {
-  sendMIDI(NOTE_ON + channel, note, DEFAULT_VELOCITY);
+void sendNoteOff(uint8_t note) {
+  sendMIDI(NOTE_OFF + state.channel, note, 0);
 }
 
 void sendNoteOff(uint8_t channel, uint8_t note) {
@@ -59,21 +176,16 @@ void sendAllNotesOff(uint8_t channel) {
 }
 
 void sendMIDI(uint8_t status, uint8_t data1, uint8_t data2) {
-  //  debugMIDI(status, data1, data2);
-  Serial.write(status);
-  Serial.write(data1);
-  Serial.write(data2);
-}
-
-void debugMIDI(uint8_t status, uint8_t data1, uint8_t data2) {
-  Serial.print(status);
-  Serial.print(", ");
-  Serial.print(data1);
-  Serial.print(", ");
-  Serial.print(data2);
-  Serial.print("\n");
-}
-
-bool isAlphaKey(byte key) {
-  return key > 48 && key < 75;
+  if (DEBUG_PS2) {
+    Serial.print(status);
+    Serial.print(", ");
+    Serial.print(data1);
+    Serial.print(", ");
+    Serial.print(data2);
+    Serial.print("\n---\n");
+  } else {
+    Serial.write(status);
+    Serial.write(data1);
+    Serial.write(data2);
+  }
 }
