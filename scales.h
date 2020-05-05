99f61bdf166d80293b4904d6caf9afdd68f33a65
Joshua Kleckner <lokua@lokua.net>
2020-05-05 00:57:10 -0500

    feat: hookup LCD

--- scales.h
+++ scales.h
@@ -12,3 +12,35 @@ extern const uint8_t DORIAN[7] = {0, 2, 3, 5, 7, 9, 10};
 extern const uint8_t PHRYGIAN[7] = {0, 1, 3, 5, 7, 8, 10};
 extern const uint8_t PHRYGIAN_DOMINANT[7] = {0, 1, 4, 5, 7, 8, 10};
 extern const uint8_t LYDIAN_DOMINANT[7] = {0, 2, 4, 6, 7, 9, 10};
+
+extern const char * SCALE_NAMES[12] = {
+  "Major",
+  "Harmonic Major",
+  "Mixolydian",
+  "Major Pentatonic",
+  "Minor",
+  "Harmonic Minor",
+  "Blues Minor",
+  "Minor Pentatonic",
+  "Dorian",
+  "Phrygian",
+  "Phrygian Dominant",
+  "Lydian Dominant"
+};
+
+extern const uint8_t ROOT_KEYS[12] = {
+  PS2_KEY_KP1,
+  PS2_KEY_KP2,
+  PS2_KEY_KP3,
+  PS2_KEY_KP4,
+  PS2_KEY_KP5,
+  PS2_KEY_KP6,
+  PS2_KEY_KP7,
+  PS2_KEY_KP8,
+  PS2_KEY_KP9,
+  PS2_KEY_NUM,
+  PS2_KEY_KP_DIV,
+  PS2_KEY_KP_TIMES
+};
+
+extern const char * NOTES[12] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
